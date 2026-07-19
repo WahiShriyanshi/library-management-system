e88a74fe788da8b31981ca48c8c7c3a003a0ed22
Shriyanshi Wahi <wahishriyanshi@gmail.com>
2026-07-20 01:49:50 +0530

    Update Library class

--- Library.java
+++ Library.java
@@ -9,6 +9,7 @@ import Entity.Shift;
 import Entity.Transaction;
 import Entity.User;
 import Exception.InvalidRequestException;
+import Exception.NewTransactionCannotBeCreatedException;
 import Exception.TransactionNotFound;
 import Exception.UserDoesNotExistsException;
 import Managment.BookManagement;
@@ -26,7 +27,6 @@ public class Library {
 		this.transactionManagement = new TransactionManagement(userManagement);
 	}
 
-	// Books Managment Functions
 	public String addBook(int bookId, String bookName, String author, String description, int price, boolean available)
 			throws InvalidRequestException {
 		return bookManagement.addBook(bookId, bookName, author, description, price, available);
@@ -70,15 +70,14 @@ public class Library {
 		return "Book availability has been updated";
 	}
 
-	// UserManagment
-	// int id, String name, LocalDate joiningDate, String email, String password
-	public String addMember(int memberid, String name, String email, String password) throws InvalidRequestException {
+	public String addMember(int memberid, String name, String email, String password)
+			throws InvalidRequestException {
 		if (memberid != 0 && name != null && email != null && password != null) {
 			Member member = new Member(memberid, name, LocalDate.now(), email, password);
 			userManagement.addUser(member);
-			return "New Member has been added with Id" + memberid;
+			return "New Member has been added with Id: " + memberid;
 		}
-		throw new InvalidRequestException("Check all the feilds correctly");
+		throw new InvalidRequestException("Check all the fields correctly");
 	}
 
 	public String addLibrarian(int userid, String name, String email, String password, Shift shift)
@@ -86,9 +85,9 @@ public class Library {
 		if (userid != 0 && name != null && email != null && password != null) {
 			Librarian librarian = new Librarian(userid, name, LocalDate.now(), email, password, shift);
 			userManagement.addUser(librarian);
-			return "New Member has been added with Id" + userid;
+			return "New Librarian has been added with Id: " + userid;
 		}
-		throw new InvalidRequestException("Check all the feilds correctly");
+		throw new InvalidRequestException("Check all the fields correctly");
 	}
 
 	public List<Librarian> showAllLibrarian() throws UserDoesNotExistsException {
@@ -107,81 +106,86 @@ public class Library {
 		return userManagement.showMemberById(memberid);
 	}
 
-	public Librarian showLibrarianById(int Librarianid) throws UserDoesNotExistsException {
-		return userManagement.showLibrarianById(Librarianid);
+	public Librarian showLibrarianById(int librarianid) throws UserDoesNotExistsException {
+		return userManagement.showLibrarianById(librarianid);
 	}
 
 	public double getPendingFine(int memberId) throws UserDoesNotExistsException {
 		return userManagement.getPendingFine(memberId);
 	}
 
-	public String removeMemberbyId(int id) throws UserDoesNotExistsException {
+	public String removeMemberById(int id) throws UserDoesNotExistsException {
 		Member member = showMemberById(id);
 		return userManagement.removeUserById(id);
 	}
 
-	public String removeLibrarianbyId(int id) throws UserDoesNotExistsException {
+	public String removeLibrarianById(int id) throws UserDoesNotExistsException {
 		Librarian librarian = showLibrarianById(id);
 		return userManagement.removeUserById(id);
 	}
 
 	public String updateMemberNameById(int id, String newname)
-			throws InvalidRequestException, UserDoesNotExistsException {
+			throws UserDoesNotExistsException {
 		Member member = userManagement.showMemberById(id);
 		member.setName(newname);
-		return "Name has been updated for id" + id + "NewNam:" + newname;
+		return "Name has been updated for id: " + id + " New Name: " + newname;
 	}
 
-	public String updateMemberEmailByid(int id, String newemail)
-			throws InvalidRequestException, UserDoesNotExistsException {
+	public String updateMemberEmailById(int id, String newemail)
+			throws UserDoesNotExistsException {
 		Member member = userManagement.showMemberById(id);
 		member.setEmail(newemail);
-		return "Name has been updated for id" + id + "NewNam:" + newemail;
+		return "Email has been updated for id: " + id + " New Email: " + newemail;
 	}
 
 	public String updateMemberPasswordById(int id, String newpassword)
-			throws InvalidRequestException, UserDoesNotExistsException {
+			throws UserDoesNotExistsException {
 		Member member = userManagement.showMemberById(id);
 		member.setPassword(newpassword);
-		return "Password has been updated for id" + id;
-	}
-
-	public String updateLibrarianPasswordById(int id, String newpassword)
-			throws InvalidRequestException, UserDoesNotExistsException {
-		Librarian librarian = userManagement.showLibrarianById(id);
-		librarian.setPassword(newpassword);
-		return "Password has been updated for id" + id;
+		return "Password has been updated for id: " + id;
 	}
 
 	public String updateLibrarianNameById(int id, String newname)
-			throws InvalidRequestException, UserDoesNotExistsException {
+			throws UserDoesNotExistsException {
 		Librarian librarian = userManagement.showLibrarianById(id);
 		librarian.setName(newname);
-		return "Name has been updated for id" + id + "NewNam:" + newname;
+		return "Name has been updated for id: " + id + " New Name: " + newname;
 	}
 
-	public String updateLibrarianEmailByid(int id, String newemail)
-			throws InvalidRequestException, UserDoesNotExistsException {
+	public String updateLibrarianEmailById(int id, String newemail)
+			throws UserDoesNotExistsException {
 		Librarian librarian = userManagement.showLibrarianById(id);
 		librarian.setEmail(newemail);
-		return "Name has been updated for id" + id + "NewNam:" + newemail;
+		return "Email has been updated for id: " + id + " New Email: " + newemail;
 	}
 
-	public String updateLibrarianShiftByid(int id, Shift shift) throws UserDoesNotExistsException {
+	public String updateLibrarianPasswordById(int id, String newpassword)
+			throws UserDoesNotExistsException {
+		Librarian librarian = userManagement.showLibrarianById(id);
+		librarian.setPassword(newpassword);
+		return "Password has been updated for id: " + id;
+	}
+
+	public String updateLibrarianShiftById(int id, Shift shift) throws UserDoesNotExistsException {
 		Librarian librarian = userManagement.showLibrarianById(id);
 		librarian.setShift(shift);
-		return "Shift has been changed for LibrarianId:" + id;
+		return "Shift has been updated for Librarian Id: " + id;
 	}
 
-	public String updateStatusIfOverdue(int transactionId) throws TransactionNotFound {
-		Transaction t = transactionManagement.getTransactionById(transactionId);
-		transactionManagement.updateStatusIfOverdue(t);
-		return "Status has been updated";
+	public String issueBook(int memberId, int bookId, int librarianId)
+			throws InvalidRequestException, UserDoesNotExistsException, NewTransactionCannotBeCreatedException {
+		Member member = userManagement.showMemberById(memberId);
+		member.setIssuedBooks(member.getIssuedBooks() + 1);
+		return transactionManagement.newTransaction(memberId, bookId, librarianId);
 	}
 
-	public double calculateFine(int transactionId) throws TransactionNotFound {
-		Transaction t = transactionManagement.getTransactionById(transactionId);
-		return transactionManagement.calculateFine(t);
+	public String returnBook(Transaction t)
+			throws TransactionNotFound, UserDoesNotExistsException, InvalidRequestException {
+		Member member = userManagement.showMemberById(t.getMemberId());
+		if (member.getIssuedBooks() <= 0) {
+			throw new InvalidRequestException("Member has no issued books to return");
+		}
+		member.setIssuedBooks(member.getIssuedBooks() - 1);
+		return transactionManagement.completeReturn(t);
 	}
-
 }
